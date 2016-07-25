67eafc392fe952fda7b19c78ccbec2d2bed5cf99
israeleriston <israeleriston@bitbucket.org>
2016-07-24 20:18:08 -0500

    mais alguns ajustes

--- src/main/java/br/com/webpublico/ponto/domain/enumeration/TipoHora.java
+++ src/main/java/br/com/webpublico/ponto/domain/enumeration/TipoHora.java
@@ -4,5 +4,24 @@ package br.com.webpublico.ponto.domain.enumeration;
  * The TipoHora enumeration.
  */
 public enum TipoHora {
-    PRIMEIRA_ENTRADA,PRIMEIRA_SAIDA,SEGUNDA_ENTRADA,SEGUNDA_SAIDA
+    PRIMEIRA_ENTRADA("Primeira Entrada"),
+    PRIMEIRA_SAIDA("Primeira Saida"),
+    SEGUNDA_ENTRADA("Segunda Entrada"),
+    SEGUNDA_SAIDA("Segunda Saida");
+
+    private String descricao;
+
+    TipoHora(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
